f543dae6231cdf01a40c3856d18a6a44192235f9
Phil Scott <me@philscotted.com>
2016-10-27 20:46:47 +0100

    LaTeX deps.

--- default.nix
+++ default.nix
@@ -4,9 +4,14 @@ let
   myemacs =
     with pkgs.emacsPackages; with pkgs.emacsPackagesNg; pkgs.emacsWithPackages
       [ org paredit haskellMode magit ];
+  tex = pkgs.texlive.combine {
+    inherit (pkgs.texlive)
+      beamer bussproofs capt-of cm-super framed ifplatform marvosym minted
+      scheme-small wasysym wrapfig xstring xcolor;
+  };
   myhaskell = pkgs.haskellPackages.ghcWithPackages (p: with p;
     [ base ]);
 in with pkgs; stdenv.mkDerivation {
-  name = "meta";
-  buildInputs = [ myemacs myhaskell ];
+  name = "proplcf";
+  buildInputs = [ myemacs myhaskell tex pythonPackages.pygments ];
 }
